fc38a3150be6cded92a515b580dd28627bd1f3d0
Mann Name <mannhp1103@outlook.com>
2020-12-07 10:23:19 -0500

    Add project files.

---
+++ Magic8Ball/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Magic8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.thinkOfquestionLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // thinkOfquestionLabel
+            // 
+            this.thinkOfquestionLabel.AutoSize = true;
+            this.thinkOfquestionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thinkOfquestionLabel.ForeColor = System.Drawing.Color.White;
+            this.thinkOfquestionLabel.Location = new System.Drawing.Point(268, 60);
+            this.thinkOfquestionLabel.Name = "thinkOfquestionLabel";
+            this.thinkOfquestionLabel.Size = new System.Drawing.Size(280, 32);
+            this.thinkOfquestionLabel.TabIndex = 0;
+            this.thinkOfquestionLabel.Text = "Think of a Question";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(87, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(610, 519);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(387, 629);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 29);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(866, 724);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.thinkOfquestionLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label thinkOfquestionLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Magic8Ball/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    public partial class Form1 : Form
+    {
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = randGen.Next(1, 7);
+
+            switch (number)
+            {
+                case 1:
+                    outputLabel.Text = "Yes";
+                    break;
+                case 2:
+                    outputLabel.Text = "No";
+                    break;
+                case 3:
+                    outputLabel.Text = "Possibly";
+                    break;
+                case 4:
+                    outputLabel.Text = "Very Doubtful";
+                    break;
+                case 5:
+                    outputLabel.Text = "Ask Again Later";
+                    break;
+                case 6:
+                    outputLabel.Text = "Best if I not say";
+                    break;
+            }
+        }
+    }
+}
